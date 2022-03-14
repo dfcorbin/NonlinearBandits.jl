d48a8524077e7398a5604202d82830826bd00408
Documenter.jl <documenter@juliadocs.github.io>
2022-03-14 10:09:14 +0000

    build based on eb895fd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NonlinearBandits","category":"page"},{"location":"#NonlinearBandits","page":"Home","title":"NonlinearBandits","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NonlinearBandits.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NonlinearBandits]","category":"page"}]
+}
