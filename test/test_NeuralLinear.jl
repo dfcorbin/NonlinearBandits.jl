32248d92ae40016cf21c603987862cf3f429b9e8
Doug Corbin <dfcorbin98@gmail.com>
2022-07-20 08:37:57 +0100

    Bug fixes

--- test/test_NeuralLinear.jl
+++ test/test_NeuralLinear.jl
@@ -30,52 +30,34 @@ function test_NeuralEncoder()
     mean_funs = (x -> 5 * sin(2 * π * x[1]), x -> 5 * cos(2 * π * x[1]))
     X = rand(n, num_inputs)
     actions = rand(1:length(mean_funs), n)
-    rewards = [mean_funs[a](x) + rand(Normal(0, 1)) for (a, x) in zip(actions, eachrow(X))]
+    rewards =
+        [mean_funs[a](x) + rand(Normal(0, 0.1)) for (a, x) in zip(actions, eachrow(X))]
     fit!(encoder, X, actions, rewards, 50, verbose = false)
 
     x = rand(1, 1)
     @test isapprox(
         predict(encoder, x),
         [mean_funs[1](x[1, 1]) mean_funs[2](x[1, 1])],
-        atol = 0.3,
+        atol = 0.5,
     )
+end
+
+function test_NeuralLinear()
+    num_inputs = 1
+    num_arms = 2
+    nl = NeuralLinear(num_inputs, num_arms, [32, 32], 20, [500 * i for i = 1:100], 50)
+
+    limits = repeat([-1.0 1.0], num_inputs, 1)
+    mf = (x -> x[1]^3, x -> x[1])
+    csampler = UniformContexts(limits)
+    rsampler = GaussianRewards(mf, 0.1)
+    metrics = (FunctionalRegret(mf),)
+    driver = StandardDriver(csampler, nl, rsampler, metrics)
+    run!(5000, 1, driver, verbose = false)
+    @test sum(metrics[1].regret[(end-100):end] .== 0.0) > 95
 end
 
 
 test_output_matrix()
 test_NeuralEncoder()
-
-
-# # # Create bandit trajectory
-# # d, n = 2, 5000
-# # mf = (x -> 3 * x[1] + 5 * x[2], x -> 4 * x[1])
-# # X = rand(d, n)
-# # a = rand(1:2, n)
-# # r = zeros(1, n)
-# # for i in 1:n
-# #     r[1, i] = mf[a[i]](X[:, i]) + rand(Normal(0, 0.1))
-# # end
-
-# # enc = NeuralEncoder(d, length(mf), [32, 32])
-# # fit!(enc, X, a, r, 10; verbose=false)
-# # Ypred = cpu(enc.nn(gpu(X)))
-# # @test mae(Ypred[1:1, :], mapslices(mf[1], X; dims=1)) < 0.1
-# # @test mae(Ypred[2:2, :], mapslices(mf[2], X; dims=1)) < 0.1
-
-# # # Test updating NeuralLinear policy appears to work
-# # limits = repeat([-1.0 1.0], d, 1)
-# # csampler = UniformContexts(limits)
-# # rsampler = GaussianRewards(mf)
-# # metrics = (FunctionalRegret(mf),)
-
-# # # Test NeuralLinear appears to be learning the optimal policy.
-# # batch_size = 1000
-# # num_batches = 3
-# # initial_batches = retrain_freq = 1
-# # nl = NeuralLinear(d, length(mf), layer_sizes, initial_batches, [1:num_batches...], 20)
-# # metrics = (FunctionalRegret(mf),)
-# # driver = StandardDriver(csampler, nl, rsampler, metrics)
-# # run!(num_batches, batch_size, driver)
-
-# # regret = metrics[1].regret[(end - batch_size + 1):end]
-# # @test sum(regret .== 0.0) / length(regret) >= 0.95
+test_NeuralLinear()
